591b0f60fa4b6c2b56b79e09cec4773d32001751
Fran <franncook17@hotmail.co.uk>
2024-08-07 10:48:18 +0000

    Commit F

--- myApp.js
+++ myApp.js
@@ -1,49 +1,68 @@
 require('dotenv').config();
 let express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 let app = express();
+let bodyParser = require("body-parser")
+
+//#1 
+console.log("Hello World")
+
+//#2 
+app.get("/", (req, res) => {
+res.send(" Hello Express")
+})
+
+// #7
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 console.log(process.env.MESSAGE_STYLE);
 
-/// #1
-//console.log("Hello World")
-
-///#2
-///app.get("/", (req,res) => {
-///res.send("Hello Express")
-///})
-
-//#4 
+// #4
 app.use("/public", express.static(__dirname + "/public"));
 
-//#3
-app.get("/", (reg,res) => {
-res.sendFile(__dirname + "/views/index.html"); 
-})
+app.use(bodyParser.urlencoded({ extended: false}))
 
-//#5 
-//app.get("/json", (req, res) => {
-//res.json ({"message": "Hello json"})
-//})
-
-//#6
-app.get("/json", function(req, res) {
-    console.log(process.env.MESSAGE_STYLE <= "message style")
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json(
-           { "message": "HELLO JSON"}
-        )
-    } else {
-        res.json({
-            "message": "Hello jason"
-        })
-    }
+// #3
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// #6
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ "message": "HELLO JSON" });
+  } else {
+    res.json({ "message": "Hello json" });
+  }
+});
 
+//#8
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({"time": req.time})
+})
 
+//#9 
+app.get("/:word/echo", (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
+});
 
+//#10 
+app.get("/name", (req, res) => {
+  res.json({ name: req.query.first + " " + req.query.last })
+})
 
-
+//#12
+app.post("/name", (req, res) => {
+  res.json({ name: req.body.first + " "+ req.body.last });
+});
 
 
 
